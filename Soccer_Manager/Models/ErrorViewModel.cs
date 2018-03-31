ad439b57e97f77338565ec7db9e7d976053a219a
Vova <klymenkowolodymyr@gmail.com>
2018-04-01 00:42:28 +0300

    First Commit

---
+++ Soccer_Manager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Soccer_Manager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
